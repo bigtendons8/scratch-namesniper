deec59f3ca92c52de3af155f28f4897cacc7950c
bigtendons8 <james.guarrella@gmail.com>
2024-03-02 15:43:14 +1100

    fixing formatting fixes

--- main.py
+++ main.py
@@ -17,13 +17,12 @@ try:
         s = requests.Session()
         r = s.get(link+word)
         if r.json()['msg'] == 'valid username':
-            print(word + ": free")
-            free.append(word + '\n')
-            count += 1
-            print(str(count) + '/' + str(len(words)) + 'complete. ' + str(len(free)) + ' names found.')
+            print(f"{word}: available")
+            free.append(f"{word}\n")
         else:
-            count += 1
-            print(str(count) + '/' + str(len(words)) + 'complete. ' + str(len(free)) + ' names found.')
+            print(f"{word}: unavailable")
+        count += 1
+        print(str(count) + '/' + str(len(words)) + 'complete. ' + str(len(free)) + ' names found.')
 except KeyboardInterrupt:
     writefree()
     raise SystemExit
