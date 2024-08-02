5d210ee84038a450a9a9ae8e9d34e928d6d1252b
bigtendons8 <154099045+bigtendons8@users.noreply.github.com>
2024-08-02 17:59:21 +1000

    use enumerate instead of count variable

--- main.py
+++ main.py
@@ -5,27 +5,24 @@ with open("list.txt", "r") as file:
     words = file.read().strip().split("\n")
 
 
-def writefree():
-    with open('output.txt', 'w') as file:
-        file.writelines(free)
-
-
 def main():
+    def writefree():
+        with open('output.txt', 'w') as file:
+            file.writelines(free)
+
     link = 'https://api.scratch.mit.edu/accounts/checkusername/'
-    count = 0
     free = []
     s = requests.Session()
 
     try:
-        for word in words:
+        for count, word in enumerate(words):
             r = s.get(link + word)
             if r.json()['msg'] == 'valid username':
                 print(f"{word}: available")
                 free.append(f"{word}\n")
             else:
                 print(f"{word}: unavailable")
-            count += 1
-            print(f"{count}/{len(words)} complete. {len(free)} names found.")
+            print(f"{count+1}/{len(words)} complete. {len(free)} names found.")
     except KeyboardInterrupt:
         writefree()
         raise SystemExit
