b9910d032ac4c41b308e246a88ef598015d61894
bigtendons8 <154099045+bigtendons8@users.noreply.github.com>
2024-08-02 17:22:00 +1000

    fix the messed up code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+
 import requests
-<<<<<<< HEAD
 
 link = 'https://api.scratch.mit.edu/accounts/checkusername/'
 count = 0
@@ -12,37 +12,10 @@ with open("list", "r") as file:
 def writefree():
     with open('output', 'w') as file:
         file.writelines(free)
-=======
-#import random
-
-link = 'https://api.scratch.mit.edu/accounts/checkusername/'
-count = 0
-
-file = open("list", "r")
-data = file.read()
-words = data.split("\n")
-file.close()
-
-#OR
-
-#words = []
-
-free = []
-
-#random.shuffle(words)
-
-def writefree():
-    file = open('output', 'a')
-    file = open('output', 'w')
-    file.writelines(free)
-    file.close()
->>>>>>> origin/main
-
 
 try:
     for word in words:
-<<<<<<< HEAD
-        r = s.get(link+word)
+        r = s.get(link + word)
         if r.json()['msg'] == 'valid username':
             print(f"{word}: available")
             free.append(f"{word}\n")
@@ -50,24 +23,8 @@ try:
             print(f"{word}: unavailable")
         count += 1
         print(f"{count}/{len(words)} complete. {len(free)} names found.")
-=======
-        s = requests.Session()
-        r = s.get(link+word)
-        if r.json()['msg'] == 'valid username':
-            print(word + ": free")
-            free.append(word + '\n')
-            count += 1
-            print(str(count)+'/'+str(len(words))+'complete. '+str(len(free))+'names found.')
-        else:
-            count += 1
-            print(str(count) + '/' + str(len(words)) + 'complete. ' + str(len(free)) + ' names found.')
->>>>>>> origin/main
 except KeyboardInterrupt:
     writefree()
     raise SystemExit
 
-<<<<<<< HEAD
 writefree()
-=======
-writefree()
->>>>>>> origin/main
